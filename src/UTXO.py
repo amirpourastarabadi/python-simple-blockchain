6590c55d8d7126de1a98219de3dfbc1787af9d3e
amirpourastarabadi <amirpourastarabadi@gmail.com>
2022-05-22 09:50:27 +0430

    add class UTXO blueprint

---
+++ src/UTXO.py
@@ -0,0 +1,20 @@
+
+class UTXO:
+
+    def __int__(self, tx_hash, index):
+        pass
+
+    def get_transaction_hash(self):
+        pass
+
+    def get_index(self):
+        pass
+
+    def equals(self, other):
+        pass
+
+    def hash_code(self):
+        pass
+
+    def compare_to(self, utxo):
+        pass
