b81603bf9bcd195ce4c331ee1838f7f931fd743b
amirpourastarabadi <amirpourastarabadi@gmail.com>
2022-05-22 09:40:02 +0430

    add class TransactionHandler blueprint

---
+++ src/TransactionHandler.py
@@ -0,0 +1,12 @@
+
+
+class TransactionHandler:
+
+    def __init__(self, utxo_pool):
+        pass
+
+    def is_valid_transaction(self, transaction):
+        pass
+
+    def handle_transactions(self, possible_transactions):
+        pass
