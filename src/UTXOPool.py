8e3eef5f524444bdf523217928ee2e8936ae2386
amirpourastarabadi <amirpourastarabadi@gmail.com>
2022-05-22 09:55:15 +0430

    add class UTXOPool blueprint

---
+++ src/UTXOPool.py
@@ -0,0 +1,21 @@
+
+
+class UTXOPool:
+
+    def __init__(self, u_pool=None):
+        pass
+
+    def add_UTXO(self, utxo, transaction_out):
+        pass
+
+    def remove_UTXO(self, utxo):
+        pass
+
+    def get_transaction_output(self, ut):
+        pass
+
+    def contains(self, utxo):
+        pass
+
+    def get_all_UTXO(self):
+        pass
